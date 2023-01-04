7d0de166b6ea96e88158881ba7bef0c62c13b195
tibogeeraerts <r0882113@student.thomasmore.be>
2023-01-04 21:05:42 +0100

    Add hashing

--- project/auth.py
+++ project/auth.py
@@ -1,20 +1,18 @@
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
 from datetime import datetime, timedelta
-
-import crud
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+import crud
 
+# OAuth2 variables
 SECRET_KEY = "0dd9755ad5b8082db4e5378b1610c29912d63ef14d630a8beecd36d6fbd2521b"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-pwd_context = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
+# Create JWT token
 def create_access_token(data: dict):
     to_encode = data.copy()
     expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -29,15 +27,16 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+# Password hashing
+pwd_context = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-
+# Login check for Admin
 def authenticate_admin(db: Session, username: str, password: str):
     admin = crud.get_admin_username(db, username)
     if not admin:
@@ -46,7 +45,7 @@ def authenticate_admin(db: Session, username: str, password: str):
         return False
     return admin
 
-
+# Check current admin
 def get_current_admin(db: Session, token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
