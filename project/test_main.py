29d089dc0d317379d12a57d6ae2a7291428b355a
tibogeeraerts <r0882113@student.thomasmore.be>
2023-01-06 13:34:40 +0100

    Change test URL's

--- project/test_main.py
+++ project/test_main.py
@@ -3,14 +3,14 @@ import json
 
 # Create test admin function
 def create_testadmin():
-    adminexists = requests.get('http://127.0.0.1:8000/admin/testadmin')
+    adminexists = requests.get('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/admin/testadmin')
     if adminexists:
         print("INFO:     Test admin already exists!")
     else:
         username = "testadmin"
         password = "testadmin"
         data = json.dumps({"username": username, "password": password})
-        requests.post('http://127.0.0.1:8000/admin', data=data)
+        requests.post('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/admin', data=data)
 
 # Create test admin
 create_testadmin()
@@ -31,7 +31,7 @@ request_data = {
 }
 
 # Requesting the token
-tokenrequest = requests.post("http://127.0.0.1:8000/token", data=request_data, headers=headers)
+tokenrequest = requests.post("https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/token", data=request_data, headers=headers)
 
 # Extracting the token that came with the response
 token = json.loads(tokenrequest.text)['access_token']
@@ -47,7 +47,7 @@ def test_post_quote():
     print("\n")
     content = "This is the post a test quote"
     data = json.dumps({"content": content})
-    response = requests.post('http://127.0.0.1:8000/quotes', data=data)
+    response = requests.post('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/quotes', data=data)
     assert response.status_code == 200
     response_dict = response.json()
     assert response_dict['content'] == content
@@ -55,7 +55,7 @@ def test_post_quote():
 # Test GET all quotes
 def test_get_all_quotes():
     print("INFO:     POST quote test passed!")
-    response = requests.get('http://127.0.0.1:8000/quotes/all')
+    response = requests.get('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/quotes/all')
     assert response.status_code == 200
     response_dict = response.json()
     assert type(response_dict[0]['content']) == str #[0] because response is a list
@@ -63,7 +63,7 @@ def test_get_all_quotes():
 # Test GET random quote
 def test_get_random_quote():
     print("INFO:     GET all quotes test passed!")
-    response = requests.get('http://127.0.0.1:8000/quotes/random')
+    response = requests.get('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/quotes/random')
     assert response.status_code == 200
     response_dict = response.json()
     assert type(response_dict['content']) == str
@@ -71,7 +71,7 @@ def test_get_random_quote():
 # Test GET last quote
 def test_get_last_quote():
     print("INFO:     GET random quote test passed!")
-    response = requests.get('http://127.0.0.1:8000/quotes/last')
+    response = requests.get('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/quotes/last')
     assert response.status_code == 200
     response_dict = response.json()
     assert type(response_dict['content']) == str
@@ -81,7 +81,7 @@ def test_put_last_quote():
     print("INFO:     GET last quote test passed!")
     content = "This is the update a test quote"
     data = json.dumps({"content": content})
-    response = requests.put('http://127.0.0.1:8000/quotes/last', data=data)
+    response = requests.put('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/quotes/last', data=data)
     assert response.status_code == 200
     response_dict = response.json()
     assert response_dict['content'] == content
@@ -89,13 +89,13 @@ def test_put_last_quote():
 # Test DELETE last quote
 def test_delete_last_quote():
     print("INFO:     PUT last quote test passed!")
-    response = requests.delete('http://127.0.0.1:8000/quotes/last')
+    response = requests.delete('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/quotes/last')
     assert response.status_code == 200
 
 # Test GET current admin
 def test_get_current_admin():
     print("INFO:     DELETE last quote test passed!")
-    response = requests.get('http://127.0.0.1:8000/admin', headers=headerswithtoken)
+    response = requests.get('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/admin', headers=headerswithtoken)
     assert response.status_code == 200
     response_dict = response.json()
     assert type(response_dict['username']) == str
@@ -107,7 +107,7 @@ def test_post_admin():
     username = "testadmin2"
     password = "testadmin2"
     data = json.dumps({"username": username, "password": password})
-    response = requests.post('http://127.0.0.1:8000/admin', data=data)
+    response = requests.post('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/admin', data=data)
     assert response.status_code == 200
     response_dict = response.json()
     assert response_dict['username'] == username
@@ -117,5 +117,5 @@ def test_print_passed_last_test():
     print("INFO:     POST admin test passed!")
 
     # Delete test admins
-    requests.delete('http://127.0.0.1:8000/admin/testadmin')
-    requests.delete('http://127.0.0.1:8000/admin/testadmin2')
+    requests.delete('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/admin/testadmin')
+    requests.delete('https://api-eindproject-service-tibogeeraerts.cloud.okteto.net/admin/testadmin2')
